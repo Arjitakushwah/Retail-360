f2a36d7c8227ee1679fc996c014ce5979f5bf025
Supreeta Roy <rsupreeta123@gmail.com>
2025-12-03 05:20:17 +0000

    commit

---
+++ retail_dbt/models/Staging/stg_supplier.sql
@@ -0,0 +1,18 @@
+
+with source as (
+    select * from {{ source('src', 'supplier') }}
+),
+
+supp as (
+    select
+        s_suppkey      as supplier_id,
+        s_name         as name,
+        s_address      as address,
+        s_nationkey    as nation_id,
+        s_phone        as phone,
+        s_acctbal      as account_balance,
+        s_comment      as comment
+    from source
+)
+
+select * from supp
